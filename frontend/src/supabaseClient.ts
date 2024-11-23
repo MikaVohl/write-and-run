0a665c54cc3bd51bba327bf3c2577b6abcf6fced
Nicklas Astorian <nicklasastorian@googlemail.com>
2024-11-22 19:10:46 -0500

    Fix

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from './types/supabase';
+// import { Database } from './types/supabase';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const supabase = createClient<any>(supabaseUrl, supabaseAnonKey);
