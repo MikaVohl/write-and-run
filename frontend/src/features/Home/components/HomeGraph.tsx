2c483eab38840519aec55c3c006111350000fc91
Mack <mackenziefaulkner@icloud.com>
2024-11-23 12:25:58 -0500

    Creation of TestMe Page and UI changes

--- frontend/src/features/Home/components/HomeGraph.tsx
+++
@@ -1,8 +0,0 @@
-
-const HomeGraph = () => {
-    return (
-        <>
-
-        </>
-    )
-};
--- frontend/src/features/Profile/index.tsx
+++ frontend/src/features/Profile/index.tsx
@@ -1,10 +1,14 @@
-
+import React, { useRef, useState } from "react";
+import { Card } from "../../components/ui/card";
 const Profile = () => {
-    return (
-        <div>
-        <h1>Profile</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Card className="bg-blue-100 w-1/2 h-1/2 p-4 rounded-lg hoverL:shadow-lg shadow-lg">
+        <h1 className="text-xl font-bold text-blue-900">Profile</h1>
+        <p className="text-blue-700">Info</p>
+      </Card>
+    </div>
+  );
+};
 
 export default Profile;
