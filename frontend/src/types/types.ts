1ac366b8da979d344085f95b4ccf23d5aa4ce449
Nicklas Astorian <nicklasastorian@googlemail.com>
2024-11-23 11:51:20 -0500

    updated implementation for updated types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -4,10 +4,10 @@ export type Tables = Database['public']['Tables'];
 
 export type Session = Tables['sessions']['Row'];
 export enum Language {
-    Python = "Python",
-    Java = "Java",
-    C = "C",
-    Bash = "Bash"
+    python = "python",
+    java = "java",
+    c = "c",
+    bash = "bash"
 }
 
 export type SessionStatus = 'pending' | 'completed' | 'failed';
