39c8fb217eaeff25e1446bc74f54975887a96c94
Karan Gandhi <karangandhi272@gmail.com>
2024-11-22 22:53:08 -0500

    sdfds

--- api/app.py
+++ api/app.py
@@ -4,8 +4,11 @@ import subprocess
 import tempfile
 import os
 import secrets
+from flask_cors import CORS, cross_origin
 import shutil
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def home():
