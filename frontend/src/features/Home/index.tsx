19f007501d69bbc0faed7646599a7b2d976858e5
Mack <mackenziefaulkner@icloud.com>
2024-11-22 23:20:41 -0500

    first one

--- frontend/src/features/Home/index.tsx
+++ frontend/src/features/Home/index.tsx
@@ -1,26 +1,21 @@
+import { Card } from "@/components/ui/card";
 import UploadComponent from "./components/Upload";
 
 const Home = () => {
-
-    return (
-        <div>
-
-            <div className="w-full h-full flex flex-col">
-                <div className="flex flex-row space-x-4">
-                    <div className="w-2/3">
-                        <UploadComponent />
-                    </div>
-
-                    <div className="w-1/3 bg-purple-900 m-4 h-32rem">
-                        test
-                    </div>
-                </div>
-                <div className="flex flex-col space-y-4">
-                    Table
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="w-full h-full flex flex-col">
+        <div className="flex flex-row justify-center">
+          <div className="w-1/2">
+            <UploadComponent />
+          </div>
         </div>
-    );
+        <div className="flex flex-row px-8 space-y-10 justify-center">
+          <Card className="w-full h-64"></Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
