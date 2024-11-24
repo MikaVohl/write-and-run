f4861a2a620dea1529c89e3b9b1c8cb76d75f904
Mika Vohl <mikavohl@gmail.com>
2024-11-23 21:07:36 -0500

    Fixed error

--- frontend/src/components/CompileCode/CompileCode.tsx
+++ frontend/src/components/CompileCode/CompileCode.tsx
@@ -11,34 +11,15 @@ const CompilerCode: React.FC<CompilerOutputProps> = ({
   compilerOutput,
   className = "h-[18rem]",
 }) => {
-  const editorOptions = {
-    readOnly: true,
-    minimap: { enabled: false },
-    automaticLayout: true,
-    scrollBeyondLastLine: false,
-    theme: 'vs-light',
-    wordWrap: 'on',
-    fontSize: 14,
-  };
-
-  // const editorOptions = {
-  //   selectOnLineNumbers: true,
-  //   automaticLayout: true,
-  //   theme: 'light',
-  //   scrollBeyondLastLine: false, // Prevents extra space after the last line
-  //   scrollbar: {
-  //     vertical: 'auto',          // Show vertical scrollbar only when necessary
-  //     horizontal: 'auto',        // Show horizontal scrollbar only when necessary
-  //     useShadows: false,         // Disable shadows around the scrollbar
-  //     verticalScrollbarSize: 8,
-  //     horizontalScrollbarSize: 8,
-  //   },
-  //   minimap: {
-  //     enabled: false,            // Disable the minimap if not needed
-  //   },
-  //   wordWrap: 'on',              // Wrap long lines to prevent horizontal scrolling
-  //   fontSize: 14,
-  // } as const;
+const editorOptions = {
+  readOnly: true,
+  minimap: { enabled: false },
+  automaticLayout: true,
+  scrollBeyondLastLine: false,
+  theme: 'vs-light',
+  wordWrap: 'on',
+  fontSize: 14,
+} as const;
 
   return (
     <div className={`${className}`}>
