6a95aa843aee9a0addd8c54ad7edbdbe51c5a43c
Mack <mackenziefaulkner@icloud.com>
2024-11-23 21:38:01 -0500

    UI

--- frontend/src/features/LandingPage/index.tsx
+++ frontend/src/features/LandingPage/index.tsx
@@ -1,26 +1,22 @@
 export default function Example() {
-
-
   return (
-    <div className="bg-white">
+    <div className="bg-white overflow-clip h-screen">
       {/* Header */}
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           aria-label="Global"
           className="flex items-center justify-between p-6 lg:px-8"
         >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+          <div className="flex lg:flex-1 p">
               <span className="sr-only">Write and Run</span>
               <img
                 src="/src/assets/Logo/W&RLogo.png"
                 alt=""
                 className="h-12 w-auto"
               />
-            </a>
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="./Home" className="text-sm/6 font-semibold text-gray-900">
+            <a href="/home" className="text-sm/6 font-semibold text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -28,7 +24,7 @@ export default function Example() {
       </header>
 
       <main className="isolate">
-        <div className="relative pt-14">
+        <div className="relative pt-1">
           <div
             aria-hidden="true"
             className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -39,23 +35,22 @@ export default function Example() {
                 <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
                   Write and Run
                 </h1>
-                <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+                <p className="mt-5 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
                   Upload your handwritten code and run it in your browser with
                   ease
                 </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
+                <div className="mt-8 flex justify-center">
                   <a
                     href="/home"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-full bg-indigo-600 px-8 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Start now
                   </a>
                 </div>
               </div>
-              <div className="mt-16 flow-root sm:mt-24">
-                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <div className="mt-16 flow-root sm:mt-12">
+                <div className="rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <img
-                    alt="App screenshot"
                     src="/src/assets/Images/ShowcaseImage.jpeg"
                     width={2432}
                     height={1442}
