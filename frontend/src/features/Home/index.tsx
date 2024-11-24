276d4cd36457edae7edd86ea0c3259e02a30201e
Mika Vohl <mikavohl@gmail.com>
2024-11-23 22:40:21 -0500

    t

--- frontend/src/features/Home/index.tsx
+++ frontend/src/features/Home/index.tsx
@@ -1,7 +1,6 @@
 import UploadComponent from "./components/Upload";
 
 const Home = () => {
-  
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <div className="flex flex-col items-center w-full">
